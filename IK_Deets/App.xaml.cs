a5d1e3939bd5a3c65e19db3a382f1034ff0cd62c
Milan Dierick <milan.dierick@gmail.com>
2021-09-18 05:05:13 +0200

    Initial commit

---
+++ IK_Deets/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace IK_Deets
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ IK_Deets/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace IK_Deets
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
