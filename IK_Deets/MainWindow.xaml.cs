fb94de8b0ba96902a03e4bac0f10550f2c7b3dab
Milan Dierick <milan.dierick@gmail.com>
2021-09-21 22:42:10 +0200

    ADD: Essential filters

--- IK_Deets/MainWindow.xaml.cs
+++ IK_Deets/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 using IK_Deets.Interfaces;
 using IK_Deets.Records;
 using MongoDB.Bson;
@@ -19,19 +20,20 @@ namespace IK_Deets
         private readonly DataTable                      _dataTable = new("PlayerDataTable");
         private readonly Database.Database              _database  = new();
         private readonly IMongoCollection<BsonDocument> _playerCollection;
-        private          Task                           _gridUpdateTask;
-        private          CancellationTokenSource        _tokenSource;
 
         public MainWindow()
         {
-            _tokenSource = new CancellationTokenSource();
-
             _playerCollection = _database.GetCollection<BsonDocument>("data", "players");
             List<BsonDocument> documents = _playerCollection.Find(new BsonDocument()).ToList();
 
-            List<int> servers = _playerCollection.Distinct<int>("server", new BsonDocument()).ToList();
+            List<int>    servers   = _playerCollection.Distinct<int>("server", new BsonDocument()).ToList();
+            List<string> alliances = _playerCollection.Distinct<string>("alliance", new BsonDocument()).ToList();
+            List<string> ranks     = _playerCollection.Distinct<string>("rank", new BsonDocument()).ToList();
 
-            // _dataTable.Columns.Add("ID");
+            servers.Insert(0, 0);
+            alliances.Insert(0, "All");
+            ranks.Insert(0, "All");
+
             _dataTable.Columns.Add("Name");
             _dataTable.Columns.Add("Server");
             _dataTable.Columns.Add("Alliance");
@@ -43,41 +45,80 @@ namespace IK_Deets
             _dataTable.Columns.Add("Dismantle Durability");
             _dataTable.Columns.Add("Time");
 
-            // _dataTable.Columns.Add("Time");
-            // _dataTable.Columns.Add("Name");
-            // _dataTable.Columns.Add("Lord Power");
-            // _dataTable.Columns.Add("Troop Power");
-
             // TODO: Create POCO mapping
-            foreach (BsonDocument document in documents)
-            {
-                _dataTable.Rows.Add(document[1], document[2], document[3], document[4], document[5], document[6],
-                                    document[7], document[8], document[9], document[10]);
-            }
+            // foreach (BsonDocument document in documents)
+            // {
+            //     _dataTable.Rows.Add(document[1], document[2], document[3], document[4], document[5], document[6],
+            //                         document[7], document[8], document[9], document[10]);
+            // }
 
             InitializeComponent();
 
-            PlayerGrid.DataContext              = _dataTable;
-            ServerSelectionComboBox.DataContext = servers;
+            PlayerGrid.DataContext                = _dataTable;
+            ServerSelectionComboBox.DataContext   = servers;
+            AllianceSelectionComboBox.DataContext = alliances;
+            RankSelectionComboBox.DataContext     = ranks;
+
+            ServerSelectionComboBox.SelectedIndex   = 0;
+            AllianceSelectionComboBox.SelectedIndex = 0;
+            RankSelectionComboBox.SelectedIndex     = 0;
+            
+            UpdatePlayerGridFilter();
         }
 
         private async void UpdatePlayerGridFilter()
         {
             // Lock all controls so that there are no issues with filter queries running concurrently
+            FilterControlsGrid.IsEnabled = false;
             
             _dataTable.Rows.Clear();
 
             FilterDefinitionBuilder<BsonDocument> filterBuilder = Builders<BsonDocument>.Filter;
             FilterDefinition<BsonDocument>        filter        = FilterDefinition<BsonDocument>.Empty;
 
-            if (ServerSelectionComboBox.Text != string.Empty)
+            if (ServerSelectionComboBox.Text != string.Empty && ServerSelectionComboBox.Text != "0")
             {
-                filter = filterBuilder.Eq("server", int.Parse(ServerSelectionComboBox.Text));
+                filter &= filterBuilder.Eq("server", int.Parse(ServerSelectionComboBox.Text));
             }
 
             if (NameSearchTextBox.Text != string.Empty)
             {
                 filter &= filterBuilder.Eq("name", NameSearchTextBox.Text);
+            }
+
+            if (AllianceSelectionComboBox.Text != string.Empty && AllianceSelectionComboBox.Text != "All")
+            {
+                filter &= filterBuilder.Eq("alliance", AllianceSelectionComboBox.Text);
+            }
+            
+            if (RankSelectionComboBox.Text != string.Empty && RankSelectionComboBox.Text != "All")
+            {
+                filter &= filterBuilder.Eq("rank", RankSelectionComboBox.Text);
+            }
+            
+            if (TroopPowerSearchTextBox.Text != string.Empty)
+            {
+                filter &= filterBuilder.Gte("troop_power", int.Parse(TroopPowerSearchTextBox.Text));
+            }
+            
+            if (HighestPowerSearchTextBox.Text != string.Empty)
+            {
+                filter &= filterBuilder.Gte("lord_power", int.Parse(HighestPowerSearchTextBox.Text));
+            }
+            
+            if (TechContributionsSearchTextBox.Text != string.Empty)
+            {
+                filter &= filterBuilder.Gte("tech_contributions", int.Parse(TechContributionsSearchTextBox.Text));
+            }
+            
+            if (DefeatSearchTextBox.Text != string.Empty)
+            {
+                filter &= filterBuilder.Gte("defeat", int.Parse(DefeatSearchTextBox.Text));
+            }
+            
+            if (DismantleDurabilitySearchTextBox.Text != string.Empty)
+            {
+                filter &= filterBuilder.Gte("dismantle", int.Parse(DismantleDurabilitySearchTextBox.Text));
             }
 
             await _playerCollection.Find(filter)
@@ -86,11 +127,12 @@ namespace IK_Deets
                                                      document[8], document[9], document[10]));
 
             _dataTable.AcceptChanges();
-            
+
             // Unlock all controls
+            FilterControlsGrid.IsEnabled = true;
         }
 
-        private void ServerSelectionComboBox_OnDropDownClosed(object sender, EventArgs e)
+        private void FilterButton_OnClick(object sender, RoutedEventArgs e)
         {
             UpdatePlayerGridFilter();
         }
